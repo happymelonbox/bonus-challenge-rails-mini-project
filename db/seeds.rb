865d7bd8622ac4d88731f09354ad4e7bc38e9be8
Justin Berger Howes <howes.j.j@gmail.com>
2021-10-16 21:51:34 +1100

    added seed - complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lists = List.create([{name: "Groceries", description: "Things to pick up from Coles"}, {name: "Chemist", description: "Things to pick up from Chemist Warehouse"}, {name: "Dry Cleaners", description: "Things to pick up from Dry cleaners"}])
+items = Item.create([{name: "Toothpaste", description: "Colegate Total Plus Whitening", quantity: 3, list_id: 2},
+    {name: "Toothpaste", description: "Colegate Total Plus Whitening", quantity: 3, list_id: 2},
+    {name: "Nurofen", description: "Ibuprofen", quantity: 1, list_id: 2},
+    {name: "Ham", description: "Virginian Shaved g", quantity: 400, list_id: 1},
+    {name: "Coffee", description: "Nescafe gold", quantity: 1, list_id: 1},
+    {name: "Zucchini", description: "Vegetable", quantity: 6, list_id: 1},
+    {name: "White Shirt", description: "White shirt for wedding", quantity: 3, list_id: 3},
+    {name: "Blazer Jacket", description: "Brown", quantity: 3, list_id: 3},
+    {name: "Wedding Dress", description: "Delecate - Check for damage", quantity: 3, list_id: 3}
+    ])
